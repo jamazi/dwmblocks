2e1f11db40ef39bfe2a5dc0b864e1da8b6bfc9e6
jamazi <31401744+jamazi@users.noreply.github.com>
2021-06-16 11:36:04 +0300

    update blocks

--- blocks.h
+++ blocks.h
@@ -3,13 +3,18 @@
 #define BLOCK(icon, bgcolor, fgcolor, cmd, interval, signal) { "printf '%s%s%s%s^d^\n' \"" fgcolor "\" \"" bgcolor "\" \"" icon "\" \"$(" cmd ")\"", interval, signal }
 
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-    {"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    /*Icon*/        /*Bg Color*/    /*Fg Color*/    /*Command*/                 /*Update Interval*/     /*Update Signal*/
+    BLOCK("",       "",             "^C1^",         "sb-volume",                5,                      1),
+    BLOCK(" ",     "",             "^C2^",         "sb-ram-usage",             2,                      2),
+    BLOCK(" ",     "",             "^C3^",         "sb-cpu-usage-temp 2",      2,                      3),
+    BLOCK("",       "",             "^C4^",         "sb-battery-status",        60,                     4),
+    BLOCK("",       "",             "^C5^",         "sb-nettraffic 1",          1,                      5),
+    BLOCK("",       "",             "^C6^",         "sb-prayers",               60,                     6),
+    BLOCK("",       "",             "^C7^",         "sb-datetime",              5,                      7),
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "︱";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
